fc512e7dbd68684bd18406c8d7bb1538f458f9b9
MosinRA <mosinra90@yandex.ru>
2020-08-16 19:43:40 +0500

    hw7 снекбар

--- app/src/main/java/com/mosin/topweather/MainActivity.java
+++ app/src/main/java/com/mosin/topweather/MainActivity.java
@@ -11,6 +11,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
     private int currentPosition = 0;
     private int indexArr = 0;
@@ -23,30 +25,28 @@ public class MainActivity extends AppCompatActivity {
         initOrientation();
         findView();
         String[] data = getResources().getStringArray(R.array.city_names);
-//        String[] dataDays = getResources().getStringArray(R.array.days_name);
 
         if (currentPosition == 1) {
             SocSource sourceData = new SocSource(getResources());
             initDaysView(sourceData.build());
             initRecyclerView(data);
             initDaysView(sourceData);
-        }
-        else initRecyclerView(data);
+        } else initRecyclerView(data);
 
-        }
+    }
 
-    public void initOrientation(){
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+    public void initOrientation() {
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
             currentPosition = 1;
         } else currentPosition = 0;
     }
 
-    public void findView(){
+    public void findView() {
         showCity = findViewById(R.id.cityNameViewFragmentShowCityInfo);
         showTempView = findViewById(R.id.showTempViewFragmentShowCityInfo);
     }
 
-    public void initRecyclerView (final String[] data){
+    public void initRecyclerView(final String[] data) {
         final RecyclerView recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
@@ -58,23 +58,29 @@ public class MainActivity extends AppCompatActivity {
         adapter.SetOnItemClickListener(new RecyclerAdapter.OnItemClickListener() {
             String[] tempArr = getResources().getStringArray(R.array.temps);
             String[] cityNamesArr = getResources().getStringArray(R.array.city_names);
+
             @Override
             public void onItemClick(View view, int position) {
                 indexArr = recyclerView.getChildLayoutPosition(view);
-                if (currentPosition == 0) {
-                    Intent intent = new Intent(MainActivity.this, ShowCitySingleModeActivity.class);
-                    intent.putExtra("cityIndex", indexArr);
-                    startActivity(intent);
-                }
-                else {
-                    showCity.setText(cityNamesArr[indexArr]);
-                    showTempView.setText(tempArr[indexArr]);
-                }
+                Snackbar.make(view, "Подтвердите действие", Snackbar.LENGTH_LONG)
+                        .setAction("сменить город", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                if (currentPosition == 0) {
+                                    Intent intent = new Intent(MainActivity.this, ShowCitySingleModeActivity.class);
+                                    intent.putExtra("cityIndex", indexArr);
+                                    startActivity(intent);
+                                } else {
+                                    showCity.setText(cityNamesArr[indexArr]);
+                                    showTempView.setText(tempArr[indexArr]);
+                                }
+                            }
+                        }).show();
             }
         });
     }
 
-    public void initDaysView (SocSource SourceData){
+    public void initDaysView(SocSource SourceData) {
         RecyclerView recyclerViewDays = findViewById(R.id.recycler_day_view);
         recyclerViewDays.setHasFixedSize(true);
         LinearLayoutManager layoutManagerDays = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
