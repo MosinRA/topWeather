3e49cce611cf623a9713dfdfda6761bd37815f5f
MosinRA <mosinra90@yandex.ru>
2020-08-23 08:42:24 +0500

    hw8

---
+++ app/src/main/java/com/mosin/topweather/model/Coord.java
@@ -0,0 +1,22 @@
+package com.mosin.topweather.model;
+
+public class Coord {
+    private float lat;
+    private float lon;
+
+    public float getLon() {
+        return lon;
+    }
+
+    public float getLat() {
+        return lat;
+    }
+
+    public void setLon(float lon) {
+        this.lon = lon;
+    }
+
+    public void setLat(float lat) {
+        this.lat = lat;
+    }
+}
